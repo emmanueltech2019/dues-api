268f98a04d176d74f71f626c7ebaaaa6d384e604
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-04-24 21:24:10 +0100

    updates

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -14,7 +14,7 @@ routes.get("/profile",requireSignin,profile)
 routes.post('/holiday-records', requireSignin, createHolidayRecord);
 
 // Route to get all holiday records
-routes.post('/holiday-records', requireSignin, getAllHolidayRecords);
+routes.post('/holiday-record', requireSignin, getAllHolidayRecords);
 
 // Route to get a single holiday record by ID
 routes.get('/holiday-records/:id', requireSignin, getHolidayRecordById);
